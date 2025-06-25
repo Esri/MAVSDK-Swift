4e1fa19efea419483efdeb0630fe55b261839991
Eric Hung <ehung@esri.com>
2025-06-25 15:00:24 -0700

    [7623] include protobuff as dependency

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
                 "MavsdkServer",
                 .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "RxBlocking", package: "RxSwift")
+                .product(name: "SwiftProtobuf", package: "SwiftProtobuf")
             ],
             exclude: [
               "proto",
