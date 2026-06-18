e41e612fcf56676ac579ad7271edf78783c3b7c4
Dmytro Malakhov <byuarus@gmail.com>
2026-06-18 16:05:29 -0700

    Fixed build issue in mavsdk-swift part2

--- Package.swift
+++ Package.swift
@@ -31,6 +31,7 @@ let package = Package(
   targets: [
     .target(name: "Mavsdk",
             dependencies: [
+                "MavsdkServer",
                 .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "RxSwift", package: "RxSwift"),
                 .product(name: "RxBlocking", package: "RxSwift")
