e51dc38cc1699a3a136640ea26b326799a94b62f
Dmytro Malakhov <byuarus@gmail.com>
2026-06-26 16:35:16 -0700

    attempt to fix issue with stalled memory storage telemetry

--- Package.swift
+++ Package.swift
@@ -48,8 +48,8 @@ let package = Package(
             ]
     ),
     .binaryTarget(name: "mavsdk_server",
-                      url: "https://github.com/Esri/MAVSDK-Swift/releases/download/1.4.0-dSymV16/mavsdk_server.xcframework.zip",
-                      checksum: "61300de875b1407a3311a9bfd9354d8a570c2f0a2e8f125a2f3da784ba9ea477"),
+                      url: "https://github.com/Esri/MAVSDK-Swift/releases/download/1.4.0-dSymV17/mavsdk_server.xcframework.zip",
+                      checksum: "fbff415804ffe0722f1336755000314f5d772f808497925679134ee9f182e764"),
     .testTarget(name: "MavsdkTests",
                 dependencies: [
                   "Mavsdk",
